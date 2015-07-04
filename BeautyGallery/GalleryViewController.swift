fa3537dadf5826bbb1d3b66c9b952ceda60f734b
Shaowei <zsw22@vip.qq.com>
2015-07-04 14:32:32 +1000

    加入分享

--- BeautyGallery/GalleryViewController.swift
+++ BeautyGallery/GalleryViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+//导入 social framework 用来社交分享功能
+import Social
 
 class GalleryViewController: UIViewController {
     
@@ -48,4 +50,19 @@ class GalleryViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // 分享按钮的action
+    @IBAction func shareTapped(sender: AnyObject) {
+        //苹果内部有内置的一些社交 app 的分享 framework command+点击 SLServiceTypeTwitter 查看内置的分享 framework
+        let controller: SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        //分享的 text
+        controller.setInitialText("一起来玩女神画廊 app 吧, 就在 Github 上")
+        //分享加图片 可以加很多张
+        controller.addImage(beautyImage.image)
+        //present view completion 完成分享后的 call back 目前是没有 所以是 nil
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    
+    
 }
